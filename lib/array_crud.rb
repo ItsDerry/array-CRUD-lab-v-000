7f6791387a9c59d77db4b16d1089af604f26b26a
Derrick Reyes <derrickreyes@me.com>
2018-08-20 20:05:31 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,8 @@ def create_an_array
   queens_of_pop = ["Madonna", "Britney", "Lady Gaga", "Rihanna"]
 end
 
-def add_element_to_end_of_array(array, element)
-
+def add_element_to_end_of_array(queens_of_pop, "Beyonce")
+  queens_of_pop << "Beyonce"
 end
 
 def add_element_to_start_of_array(array, element)
